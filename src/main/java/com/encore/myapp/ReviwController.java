38863b2c660a8ac048b497d867c6591d82b31657
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-27 10:45:44 +0900

    Revert "18.08.26 : request.setAttribute("person", p); 삭제"
    This reverts commit a593f7c0894644815862571f9a2493772af10774.

--- src/main/java/com/encore/myapp/ReviwController.java
+++ src/main/java/com/encore/myapp/ReviwController.java
@@ -16,6 +16,7 @@ public class ReviwController {
 	}
 	@RequestMapping("re_result")
 	public String result(Person p, HttpServletRequest request) {
+		request.setAttribute("person", p);
 		return "review/re_result";
 	}
 	

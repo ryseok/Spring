1150a446cf45317a4e02293c2ed3e2cbe0c8f63f
ryseok <40780816+ryseok@users.noreply.github.com>
2018-09-14 19:11:45 +0900

    180914 : 지도 API Test

---
+++ src/main/java/com/encore/control/GoogleMapTestController.java
@@ -0,0 +1,12 @@
+package com.encore.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GoogleMapTestController {
+	@RequestMapping("GoogleMapTest")
+	public void GoogleMapTest() {
+		
+	}
+}
